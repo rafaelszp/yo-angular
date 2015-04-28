105d2aa926c5e5b64028a8bdfc911f8915979e53
Rafael Pereira <rafaelsouza@sistemafieg.org.br>
2015-04-28 16:08:05 -0300

    yo angular generate :D

--- vagrant/modules/nodejs/manifests/yeoman.pp
+++ vagrant/modules/nodejs/manifests/yeoman.pp
@@ -2,8 +2,22 @@ class nodejs::yeoman {
 	
 	exec {
 		["install-yeoman"]: 
-			unless => "/usr/bin/ls /tmp/yeoman.done | grep yeoman.done ",
+			unless 	=> "/usr/bin/ls /tmp/yeoman.done | grep yeoman.done ",
 			command => "/usr/bin/npm install -g yo bower grunt-cli gulp && /usr/bin/touch /tmp/yeoman.done",
+			require => Package["npm"],
+	}
+
+	exec {
+		["install-generator-webapp"]:
+			unless 	=> "/usr/bin/ls /tmp/generator-webapp.done | grep generator-webapp.done ",
+			command => "/usr/bin/npm install -g generator-webapp && /usr/bin/touch /tmp/generator-webapp.done",
+			require => Package["npm"],
+	}
+
+	exec {
+		["install-generator-angular"]:
+			unless 	=> "/usr/bin/ls /tmp/generator-angular.done | grep generator-angular.done ",
+			command => "/usr/bin/npm install -g generator-angular && /usr/bin/touch /tmp/generator-angular.done",
 			require => Package["npm"],
 	}
 
